73ea35f134da1765d147590010fc01ddc26826d4
claraMunho <claramugo@gmail.com>
2022-04-22 14:59:26 +0200

    Add files via upload

---
+++ classes/classPlant.py
@@ -0,0 +1,39 @@
+class Plant:
+    
+    numberPlants = 0
+    @classmethod
+    def countPlants(cls):
+        cls.numberPlants += 1
+        return cls.numberPlants
+
+    def __init__(self, plantName, waterIntake, *uses):
+        self._numberPlants = Plant.countPlants()
+        self._plantName = plantName
+        self._waterIntake = waterIntake
+        self._uses = list(uses)
+    def __str__(self):
+        return f'Name: {self._plantName}. Water intake: {self._waterIntake} liters per month. Uses: {self._uses}'
+    @property
+    def plantName(self):
+        return self._plantName
+    @plantName.setter
+    def plantName(self, plantName):
+        self._plantName = plantName
+    @property
+    def waterIntake(self):
+        return self._waterIntake
+    @waterIntake.setter
+    def waterIntake(self, waterIntake):
+        self._waterIntake = waterIntake
+    @property
+    def uses(self):
+        return self._uses
+    @uses.setter
+    def uses(self, uses):
+        self._uses = uses
+        
+    # def seePlantDetails(self): #No need for this because we have set the string method
+    #     print(f'Name: {self._plantName}. Water intake: {self._waterIntake} liters per month. Uses: {self._uses}')
+
+    def addUses(self, use):
+        self._uses.append(use)
